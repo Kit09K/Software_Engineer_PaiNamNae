c9955a9df3a44cc54de976425ededd76d639c4a8
Ekkasit-Ph <Ekkasit.ph@kkumail.com>
2026-02-16 01:36:12 +0700

    fix: correct import path for delete routes and update route path for soft delete user

--- backend/src/routes/delete.routes.js
+++ backend/src/routes/delete.routes.js
@@ -5,6 +5,6 @@ const DeleteController = require("../controllers/delete.controller");
 const { protect, requireAdmin } = require('../middlewares/auth');
 
 // POST /api/delete/account
-router.post("/account", protect, DeleteController.softDeleteUser);
+router.post("/delete/account", protect, DeleteController.softDeleteUser);
 
 module.exports = router;
