ac5d6e4cecf4313290064519d384c806a399d368
“Sittiphat” <sittiphat.t@kkumail.com>
2026-03-01 00:20:45 +0700

    API connection between Frontend and Backend

--- code/backend/src/services/email.service.js
+++ code/backend/src/services/email.service.js
@@ -11,6 +11,9 @@ class EmailService {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
             },
+            tls: {
+                rejectUnauthorized: false
+            }
         });
     }
 
