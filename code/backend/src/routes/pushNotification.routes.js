1405e53b31621c83e1f57784a76705b0b83a8a33
Ekkasit-Ph <Ekkasit.ph@kkumail.com>
2026-03-14 16:14:45 +0700

    Add authentication middleware to subscription and sendPush routes

--- code/backend/src/routes/pushNotification.routes.js
+++ code/backend/src/routes/pushNotification.routes.js
@@ -3,15 +3,16 @@ const router = express.Router();
 
 const PushNotificationController = require('../controllers/pushNotification.controller');
 const PushNotificationService = require('../services/pushNotification.service');
+const { protect } = require('../middlewares/auth');
 
 // สร้าง instance ของ Service และ Controller
 const pushNotificationService = new PushNotificationService();
 const pushNotificationController = new PushNotificationController(pushNotificationService);
 
 // รับข้อมูล Subscription จากหน้าเว็บ
-router.post('/subscribe', pushNotificationController.subscribe);
+router.post('/subscribe',protect, pushNotificationController.subscribe);
 
 // [เพิ่มใหม่] ยิง Push Notification (เหมาะสำหรับ Admin หรือใช้ทดสอบ)
-router.post('/send', pushNotificationController.sendPush);
+router.post('/send', protect, pushNotificationController.sendPush);
 
 module.exports = router;
